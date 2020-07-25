40315c9d87b4564a9c0067b9d30d42ea25f250e6
Himanshu Mahawar <himanshu27.stU@gmail.com>
2020-07-25 15:43:02 +0530

    Server issue resolved.

--- server/server.js
+++ server/server.js
@@ -2,14 +2,16 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
 
-app.use(express.static(publicPath));
+
+const PUBLIC_URL = path.join(__dirname, '..', 'build');
+
+app.use(express.static(PUBLIC_URL));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+    res.sendFile(path.join(PUBLIC_URL, 'index.html'));
 });
 
 app.listen(port, () => {
     console.log(`Server is up on the port: ${port}!`);
-})
+});
